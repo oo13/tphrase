e0cdd375ed671e9e7c4fe5a6bc81ab310ab96c3d
OOTA, Masato <ooyooxei+gh@gmail.com>
2025-04-15 22:18:20 +0900

    Change the license.

--- src/DataGsubs.cpp
+++ src/DataGsubs.cpp
@@ -2,7 +2,7 @@
     \file DataGsubs.cpp
     \author OOTA, Masato
     \copyright Copyright © 2024 OOTA, Masato
-    \par License GPL-3.0-or-later
+    \par License GPL-3.0-or-later or Apache-2.0
     \parblock
       This file is part of TPhrase.
 
@@ -18,6 +18,20 @@
 
       You should have received a copy of the GNU General Public License
       along with TPhrase.  If not, see <http://www.gnu.org/licenses/>.
+
+      OR
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use TPhrase except in compliance with the License.
+      You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
     \endparblock
 */
 
