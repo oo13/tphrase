5d34a5cdcadb365192b033b96ae2913bbad8dfce
OOTA, Masato <ooyooxei+gh@gmail.com>
2025-04-19 19:41:28 +0900

    Fix the license notice in test.

--- test/UnitTest.h
+++ test/UnitTest.h
@@ -1,5 +1,7 @@
 /* Class for Unit Test
 
+   Copyright © 2024 OOTA, Masato
+
    This file is part of TPhrase.
 
    TPhrase is free software: you can redistribute it and/or modify
@@ -15,7 +17,19 @@
    You should have received a copy of the GNU General Public License
    along with TPhrase.  If not, see <http://www.gnu.org/licenses/>.
 
-   Copyright © 2024 OOTA, Masato
+   OR
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use TPhrase except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
 #ifndef TEST_UNITTEST_H_
